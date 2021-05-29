3850c0354689de60408d5fb125960c20c7a79055
devlew <lewisd@bu.edu>
2021-05-29 19:23:19 -0400

    Upload solution

---
+++ caesar.rb
@@ -0,0 +1,19 @@
+def alpha?(char)
+  char.between?('A', 'Z') || char.between?('a', 'z')
+end
+
+def rotate(char, num)
+  if !([char] & ['.', '?', '!', '\'', '\"', ' ']).empty?
+    char
+  else
+    rot_ord = (char.ord + (num % 26))
+    rot_ch = rot_ord.chr
+    rot_ch = (rot_ord - 26).chr unless alpha?(rot_ch)
+    rot_ch
+  end
+end
+
+
+def encode(str, num)
+  str.each_char.reduce(''){ |caesar_str, c| caesar_str + rotate(c, num) }
+end
